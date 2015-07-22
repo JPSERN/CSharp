0d935d1704cdb3cc007e477b4d7e48c1d3055e84
tomita-ychr <sunrisedigitaltomita@gmail.com>
2015-07-22 12:32:58 +0900

    入力値チェックテスト
    if((checkString != null) && (checkString.Length != 0))が
    (String.IsNullOrEmpty(checkString) == false)だけで済むみたい。
    これは便利そう。

--- form.aspx.cs
+++ form.aspx.cs
@@ -14,6 +14,9 @@ public partial class FormTest : System.Web.UI.Page
 
     protected void Button1_OnClick(object sender, EventArgs e)
     {
-        Label1.Text = TextBox1.Text;
+        if (String.IsNullOrEmpty(TextBox1.Text) == false)
+        {
+            Label1.Text = TextBox1.Text;
+        }
     }
 }
