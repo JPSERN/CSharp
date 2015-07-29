9e65ae71cd016b17f20a666fd0f0f81bb5817e28
tomita-ychr <sunrisedigitaltomita@gmail.com>
2015-07-29 11:45:09 +0900

    とりあえずフォームのname属性を指定してひととおりパラメータ取得
    ただ、Request.Form.Get の戻り値の型が string 一択なので、
    PHPみたいに配列で取得したい場合はどうしたものか。
    取得してから配列にするしかないのか

--- form.aspx.cs
+++ form.aspx.cs
@@ -12,5 +12,7 @@ public partial class FormTest : System.Web.UI.Page
     {
         Debug.WriteLine("フォームテストなう");
         Debug.WriteLine(Request.Form.Get("textbox"));
+        Debug.WriteLine(Request.Form.Get("area"));
+        Debug.WriteLine(Request.Form.Get("lang"));//チェックボックスの値全部取れるけど、string なのよね。。。
     }
 }
