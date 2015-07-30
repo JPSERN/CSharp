6e953f6692dee2218ee0ea5f16690b082f2d7199
tomita-ychr <sunrisedigitaltomita@gmail.com>
2015-07-30 11:48:47 +0900

    アップロードされたファイルを開くテスト中
    まだ途中
    ページ読み込み時は何もアップロードされているファイルなくて
    例外エラーになる(Page_Loadに書いているせいだが)ので
    入力値チェックを付ける

--- form.aspx.cs
+++ form.aspx.cs
@@ -14,5 +14,13 @@ public partial class FormTest : System.Web.UI.Page
         Debug.WriteLine(Request.Form.Get("textbox"));
         Debug.WriteLine(Request.Form.Get("area"));
         Debug.WriteLine(Request.Form.Get("lang"));//チェックボックスの値全部取れるけど、string なのよね。。。
+
+        //アップロードしたテキストファイルを開く
+        //テキストファイル以外はどうするんだと思うがとりあえずお試しで
+        System.IO.StreamReader reader = new System.IO.StreamReader(
+            Request.Files.Get("file").InputStream
+        );
+        String text = reader.ReadToEnd();
+        Debug.Write(text);
     }
 }
